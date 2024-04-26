69339a89c5c613f980b746c085202322d952b473
lab04 <lab04@lab4-12>
2024-04-25 22:29:14 -0300

    Adicionado um if

--- ProjetoGit/src/ClassePrincipal.java
+++ ProjetoGit/src/ClassePrincipal.java
@@ -4,6 +4,12 @@ public class ClassePrincipal {
 	public static void main(String[] args) {
 		
 		System.out.print("Hello, world!!");
+		
+		int idade = 18;
+		
+		if(idade > 18) {
+			System.out.print("Okay!");
+		}
 
 	}
 
